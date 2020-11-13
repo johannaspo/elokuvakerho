aa8f2abcc88d9bb859063afc116bddc02e8336c3
Johanna <jespoof@outlook.com>
2020-11-13 18:47:11 +0200

    admin-sivut, admin-ominaisuudet ja uudelleennimeämisiä

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 """ hash_value = generate_password_hash('password')
-sql = "INSERT INTO member (username, password) VALUES (:username, :password)"
+sql = "INSERT INTO members (username, password) VALUES (:username, :password)"
 db.session.execute(sql, {"username":'username', "password":hash_value})
 db.session.commit() """
 
@@ -24,7 +24,7 @@ def login():
     username = request.form["username"]
     password = request.form["password"]
    
-    sql = "SELECT password FROM member WHERE username=:username"
+    sql = "SELECT password FROM members WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()    
     if user == None:
@@ -42,12 +42,12 @@ def logout():
     del session["username"]
     return redirect("/")
 
-@app.route('/new')
-def new():
-    return render_template('new.html')
+@app.route('/add_film')
+def add_film():
+    return render_template('add_film.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
+@app.route('/submit_film', methods=['POST'])
+def submit_film():
     if request.method == 'POST':
         name = request.form['name']
         genre = request.form['genre']
@@ -55,68 +55,117 @@ def submit():
         description = request.form['description']
         
         if name == '' or genre == '' or release_year == '' or description == '':
-            return render_template('new.html', message='Täytä kaikki kentät')
+            return render_template('add_film.html', message='Täytä kaikki kentät')
         
-        sql = 'INSERT INTO film (name, genre, release_year, description) ' \
+        sql = 'INSERT INTO films (name, genre, release_year, description) ' \
               'VALUES (:name, :genre, :release_year, :description)'
         
         db.session.execute(sql, {'name':name, 'genre':genre, 'release_year':release_year, 'description':description})
         db.session.commit()
         return render_template('success.html')
 
-@app.route('/movies')
-def movies():
-    sql = "SELECT id, name, release_year FROM film ORDER BY name ASC"
+@app.route('/films')
+def films():
+    sql = "SELECT id, name, release_year FROM films ORDER BY name ASC"
     result = db.session.execute(sql)
-    movies = result.fetchall()
-    return render_template("movies.html", movies=movies)
+    films = result.fetchall()
+    return render_template("films.html", films=films)
 
-@app.route("/movie/<int:id>")
-def movie(id):
-    sql = "SELECT name FROM film WHERE id=:id"
+@app.route("/film/<int:id>")
+def film(id):
+    sql = "SELECT name FROM films WHERE id=:id"
     result = db.session.execute(sql, {"id":id})
     name = result.fetchone()[0]
-    sql = "SELECT genre FROM film WHERE id=:id"
+    sql = "SELECT genre FROM films WHERE id=:id"
     result = db.session.execute(sql, {"id":id})
     genre = result.fetchone()[0]
-    sql = "SELECT release_year FROM film WHERE id=:id"
+    sql = "SELECT release_year FROM films WHERE id=:id"
     result = db.session.execute(sql, {"id":id})
     release_year = result.fetchone()[0]
-    sql = "SELECT description FROM film WHERE id=:id"
+    sql = "SELECT description FROM films WHERE id=:id"
     result = db.session.execute(sql, {"id":id})
     description = result.fetchone()[0]
-    sql = "SELECT member_id FROM film WHERE id=:id"
+    sql = "SELECT member_id FROM films WHERE id=:id"
     result = db.session.execute(sql, {"id":id})
     if result.fetchone()[0] == None:
         available = 'Ei'
         button = 'yes'
-        return render_template("movie.html", id=id, name=name, genre=genre, release_year=release_year, description=description, available=available, button=button)
+        return render_template("film.html", id=id, name=name, genre=genre, release_year=release_year, description=description, available=available, button=button)
     else:
         available = 'Kyllä'
-        return render_template("movie.html", id=id, name=name, genre=genre, release_year=release_year, description=description, available=available)
+        return render_template("film.html", id=id, name=name, genre=genre, release_year=release_year, description=description, available=available)
 
-@app.route('/movie/<int:id>/loan', methods=['POST'])
-def loan(id):
+@app.route('/loan', methods=['POST'])
+def loan():
     if request.method == 'POST':
-        
+        id = request.form['loan_film']
         username = session['username']
-        sql = 'UPDATE film SET (member_id) = (SELECT id FROM member WHERE username =:username) WHERE id =:id'
+        sql = 'UPDATE films SET (member_id) = (SELECT id FROM members WHERE username =:username) WHERE id =:id'
         db.session.execute(sql, {"username":username, "id":id})
         db.session.commit()
         return render_template('reservation.html')
     
 @app.route("/member/<username>")
 def member(username):
-    sql = "SELECT name FROM member WHERE username=:username"
+    sql = "SELECT name FROM members WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
     name = result.fetchone()[0]
-    sql = "SELECT email FROM member WHERE username=:username"
+    sql = "SELECT email FROM members WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
     email = result.fetchone()[0]
-    sql = "SELECT id FROM member WHERE username=:username"
+    sql = "SELECT id FROM members WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
     id = result.fetchone()[0]
-    sql = "SELECT name FROM film WHERE member_id=:id "
+    sql = "SELECT name FROM films WHERE member_id=:id "
     result = db.session.execute(sql, {"id":id})
-    movies = result.fetchall()
-    return render_template("member.html", id=id, name=name, username=username, email=email, movies=movies)
+    films = result.fetchall()
+    return render_template("member.html", id=id, name=name, username=username, email=email, films=films)
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/members')
+def members():
+    sql = "SELECT username FROM members ORDER BY name ASC"
+    result = db.session.execute(sql)
+    members = result.fetchall()
+    return render_template("members.html", members=members)
+
+@app.route('/add_member')
+def add():
+    return render_template('add_member.html')
+
+@app.route('/submit_member', methods=['POST'])
+def submit_member():
+    if request.method == 'POST':
+        name = request.form['name']
+        username = request.form['username']
+        password = generate_password_hash(request.form['password'])
+        email = request.form['email']
+        
+        if name == '' or username == '' or password == '' or email == '':
+            return render_template('add_member.html', message='Täytä kaikki kentät')
+        
+        sql = 'INSERT INTO members (name, username, password, email) ' \
+              'VALUES (:name, :username, :password, :email)'
+        
+        db.session.execute(sql, {'name':name, 'username':username, 'password':password, 'email':email})
+        db.session.commit()
+        return render_template('success.html')
+
+@app.route('/loans')
+def loans():
+    sql = "SELECT id, name, release_year FROM films WHERE member_id IS NOT NULL ORDER BY name ASC"
+    result = db.session.execute(sql)
+    films = result.fetchall()
+    return render_template("loans.html", films=films)
+
+@app.route('/return_loan', methods=['POST'])
+def return_loan():
+    if request.method == 'POST':
+        id = request.form["id"]
+        sql = "UPDATE films SET member_id = NULL WHERE id=:id"
+        db.session.execute(sql, {"id":id})
+        db.session.commit()
+        return render_template('success.html')
