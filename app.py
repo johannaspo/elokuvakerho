49fdd4a9a4875e9247374ac71b7444c70e5ef248
Johanna <jespoof@outlook.com>
2020-11-13 22:52:56 +0200

    päivämäärän muokkaus

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def reviews(id):
     sql = "SELECT name FROM films WHERE id=:id"
     result = db.session.execute(sql, {"id":id})
     name = result.fetchone()[0]
-    sql = "SELECT DATE_TRUNC('second', timestamp::timestamp) as timestamp, username, stars, text FROM reviews WHERE film_id=:id ORDER BY timestamp DESC"
+    sql = "SELECT TO_CHAR(timestamp, 'DD/MM/YYYY') as timestamp, username, stars, text FROM reviews WHERE film_id=:id ORDER BY timestamp DESC"
     result = db.session.execute(sql, {"id":id})
     reviews = result.fetchall()
     sql = "SELECT COUNT(stars) FROM reviews WHERE film_id=:id"
